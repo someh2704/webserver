c6fb064b61020b2d7ddc7f84c32d2e05b261cf32
someh2704 <someh2705@gmail.com>
2020-12-22 00:17:14 +0900

    Express 8080 port

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'html')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'hello.html'));
+});
+
+app.listen(8080, () => {
+    console.log('Express App on port 8080');
+})
